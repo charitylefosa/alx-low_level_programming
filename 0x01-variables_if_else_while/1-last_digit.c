4fd322319a5dd49ce484b0645207ec601ddf7be6
charitylefosa <charitylefosa@gmail.com>
2023-03-17 16:18:24 -0700

    if, else if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
