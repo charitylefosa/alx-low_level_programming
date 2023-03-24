dec90921c5a7b324793108ca1506804569346520
charitylefosa <charitylefosa@gmail.com>
2023-03-24 10:01:01 -0700

    li

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include<stdio.h>
+
 /**
-* print_numbers - print numbers 0 to 9
-* Return: void
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
 */
 
 void print_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-	putchar(c);
+	_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
