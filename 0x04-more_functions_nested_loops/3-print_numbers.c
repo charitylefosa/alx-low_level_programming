a92c2a43a1e09661a4d97d738be5766c2b198444
charitylefosa <charitylefosa@gmail.com>
2023-03-23 19:17:24 -0700

    l

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-	_putchar(c);
+	putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
