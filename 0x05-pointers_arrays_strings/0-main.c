750b8041f31476d21c3482be1540fb354529edad
Charitylefosa <charitylefosa@gmail.com>
2023-03-27 15:43:54 +0000

    98

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
- */
+* main - check the code
+* reset_to_98: resetting n to 98
+* Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	n = 402;
 	printf("n=%d\n", n);
-	reset_to_98(&n);
+	n = 98;
 	printf("n=%d\n", n);
 	return (0);
 }
