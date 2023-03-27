6bd487c10081b386df36cb0241354ea4a5b7219b
Charitylefosa <charitylefosa@gmail.com>
2023-03-27 16:48:24 +0000

    reset

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-* reset_to_98 - points to 98
-* Return: Always 0.
+* reset_to_98 - points updated value to 98
+* @n: variable for pointer
 */
 void reset_to_98(int *n)
 {
 	*n = 98;
-
 }
 
