5d4d6582763a719649f7d2d242c124667e62046c
Charitylefosa <charitylefosa@gmail.com>
2023-03-27 22:50:49 +0000

    string characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+* puts2 - Prints one char out of two of a string.
+* @str: The string containing characters.
+*/
+void puts2(char *str)
+{
+	int i = 0, l = 0;
+
+	while (str[i++])
+		l++;
+
+	for (i = 0; i < l; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
