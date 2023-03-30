339ba0d122cc2c52132f3bdfaf81136e56d9b097
Charitylefosa <charitylefosa@gmail.com>
2023-03-30 16:38:36 +0000

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,14 +15,13 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[i] != '\0')
 	{
-		dest[i] = src[j];
-		while (src[j] != '\0')
-		{
-			i++;
-			j++;
-		}	
-	dest[i] = src[j];
+		i++;
 	}
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	dest = src;
 	putchar('\n');
-	return (dest);
+	return (0);
 }
