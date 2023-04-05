eb362039c6924ceed557de1c33f72dd0f53932cf
charitylefosa <charitylefosa@gmail.com>
2023-04-05 15:51:22 -0700

    Update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,11 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		putchar(*s);
-		_puts_recursion(s);
-		s = s + 1;
-	}
-	else
 		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
