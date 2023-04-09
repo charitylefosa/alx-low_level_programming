2a8e497279eb09e61f29abcb3cdbd6507d0e215b
charitylefosa <charitylefosa@gmail.com>
2023-04-09 21:31:50 +0000

    today

---
+++ 0x07-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+*_strcmp - compares the two strings, s1 and s2
+*@s1: first string to be compared
+*@s2: second string to be compared
+*Return: If s1 == s2, return (0)
+*	If s1 < s2, return (<0)
+*	If s1 > s2, return (>0)
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
