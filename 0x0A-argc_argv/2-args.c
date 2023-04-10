4c73d3d7d7afeaec24bef7806d07ccb2636514d2
charitylefosa <charitylefosa@gmail.com>
2023-04-10 23:33:00 +0000

    i gat this

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - entry point
+*@argv: argument vector
+*@argc: argument count
+*Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
