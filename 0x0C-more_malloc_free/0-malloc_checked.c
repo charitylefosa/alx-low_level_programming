4dbcadd3441e47c1fee08d4c41a453e5879bee56
charitylefosa <charitylefosa@gmail.com>
2023-04-13 21:22:06 +0000

    save

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+*
+*
+*
+*
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
