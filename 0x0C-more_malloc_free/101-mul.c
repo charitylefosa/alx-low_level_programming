a5d2378bece951907b01d45d179b415b94ff17f1
charitylefosa <charitylefosa@gmail.com>
2023-04-14 02:08:44 +0000

    learn

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main- entry point
+*@argc:argument count
+*@argv:argument vector
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char command[100];
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 || num2 == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	sprintf(command, "echo 'scale=10; %d * %d' | bc", num1, num2);
+	system(command);
+	return (0);
+}
