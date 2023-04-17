3d5837a3d2f47a1e4ff46ada6cd82030c3992712
charitylefosa <charitylefosa@gmail.com>
2023-04-17 23:05:51 +0000

    Attempt 1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"dog.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+*new_dog- creates a new dog
+*@name: new dog name
+*@age: new dog age
+*@owner: new dog owner
+*Return: new_dog
+*	NULL if function fails
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+
+	n_dog = malloc(sizeof(dog_t));
+	if (n_dog != NULL)
+	{
+		n_dog->name = strdup(name);
+		n_dog->age = age;
+		n_dog->owner = strdup(owner);
+		return (n_dog);
+	}
+	else
+		return (NULL);
+}
