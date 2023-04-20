5dd2531cc7e1003324f1a733b1bd580cb6b6e196
charitylefosa <charitylefosa@gmail.com>
2023-04-20 02:51:49 +0000

    attempt 2

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,27 +2,28 @@
 #include<stdlib.h>
 #include<string.h>
 /**
-*
-*
-*
+*print_opcodes- prints opcodes
+*@s: pointer to opcodes
+*@size: size of opcodes
 *
 */
 void print_opcodes(unsigned char *s, size_t size)
 {
 	size_t i;
 
-	for (i =0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("%02x", s[i]);
 	}
 	printf("\n");
 }
 /**
-*
-*
-*
+*main- entry point
+*@argc: argument count
+*@argv: argument vector
+*Return: Always 0
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int size;
 
@@ -32,12 +33,13 @@ int main (int argc, char *argv[])
 		return (1);
 	}
 	size =  atoi(argv[1]);
-	if(size < 0)
+	if (size < 0)
 	{
 		printf("Error\n");
 		return (2);
 	}
 	unsigned char *s;
+
 	s = (unsigned char *)main;
 	return (0);
 }
