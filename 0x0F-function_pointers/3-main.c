f3469d359d1327a9461199914fe56b11af4eefcb
charitylefosa <charitylefosa@gmail.com>
2023-04-20 02:26:10 +0000

    save

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,12 +26,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (*op == '/' && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	if (*op == '%' && num2 == 0)
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
