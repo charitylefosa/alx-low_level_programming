34854c7153e794360384a4620f8aaf2398ce3374
charitylefosa <charitylefosa@gmail.com>
2023-04-20 02:08:04 +0000

    Attempt

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,49 +5,49 @@
 *op_add - add two integers
 *@a: integer 1
 *@b: integer 2
-*Return : sum of integers
+* Return: sum of a and b
 */
 int op_add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
 /**
 *op_sub - subtraction between two integers
 *@a: integer 1
 *@b: integer 2
-*Return : difference of integers
+* Return: difference of a and b
 */
 int op_sub(int a, int b)
 {
-	return (a-b);
+	return (a - b);
 }
 /**
 *op_mul- multiplication of two integers
 *@a: integer 1
 *@b: integer 2
-*Return : product of integers
+* Return: product of a and b
 */
 int op_mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
 /**
 *op_div- devision between two integers
 *@a: integer 1
 *@b: integer 2
-*Return : result of division of integers
+* Return: result of division of a and b
 */
 int op_div(int a, int b)
 {
-	return(a/b);
+	return (a / b);
 }
 /**
 *op_mod- reminder after deviding two integers
 *@a: integer 1
 *@b: integer 2
-*Return : remainder of the division of integers
+* Return: remainder of the division of a and b
 */
 int op_mod(int a, int b)
 {
-	return (a%b);
+	return (a % b);
 }
