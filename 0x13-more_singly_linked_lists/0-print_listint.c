d28d253de458c2be767c9d547ae6e8a1c67a2411
charitylefosa <charitylefosa@gmail.com>
2023-05-03 13:37:11 +0000

    Attempt 4

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
 *print_listint- prints all the elements of a listint_t list
-*@h: pointer to the listint_t list 
+*@h: pointer to the listint_t list
 *Return: number of nodes
 */
 size_t print_listint(const listint_t *h)
