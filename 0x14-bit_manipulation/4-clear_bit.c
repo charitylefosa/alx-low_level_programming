ca914df1a35d4d74e8ec6db29b67b1e75e13cc7c
charitylefosa <charitylefosa@gmail.com>
2023-07-06 19:20:09 +0000

    attempt 3

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*clear_bit- sets the value of bit to 0 at  given index
+*clear_bit - sets the value of bit to 0 at given index
 *@n: unsigned long int
 *@index: index of the bit
 *
 *Return: 1 if it worked
-*	-1 if an error occured 
+*	-1 if an error occured
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
