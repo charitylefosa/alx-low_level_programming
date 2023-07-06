bf481fafd92a6beed561e5ed9bf4a73878576269
charitylefosa <charitylefosa@gmail.com>
2023-07-06 20:08:50 +0000

    attempt 5

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,20 @@
 */
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int num;
 
-	if (*b == '\0')
-		return (0);
-	for (len = 0; b[len];)
-		len++;
-	for (len -= 1; len >= 0; len--)
+	num = 0;
+	if (!b)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		return (0);
+	}
+	while (*b)
+	{
+		num <<= 1;
+		if (*b++ == '1')
+		{
+			num |= 1;
+		}
 	}
 	return (num);
 }
