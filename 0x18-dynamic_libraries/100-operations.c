1eb922c7470c4f13c2b860b64c15cb4f11bb36e6
charitylefosa <charitylefosa@gmail.com>
2023-08-21 19:04:04 +0000

    call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+*add- adding 2 int
+*@a:1st int
+*@b:2nd int
+*Return: Sum
+*/
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+*add- subtracting 2 int
+*@a:1st int
+*@b:2nd int
+*Return: Difference
+*/
+int sub(int a, int b)
+{
+        return a - b;
+}
+
+/**
+*add- multiplying 2 int
+*@a:1st int
+*@b:2nd int
+*Return: Product
+*/
+int mul(int a, int b)
+{
+        return a * b;
+}
+
+/**
+*div- dividing 2 int
+*@a:1st int
+*@b:2nd int
+*Return: dividend 
+*/
+int div(int a, int b)
+{
+        return a / b;
+}
+
+/**
+*add- module a in b
+*@a:1st int
+*@b:2nd int
+*Return: module
+*/
+int mod(int a, int b)
+{
+        return a % b;
+}
