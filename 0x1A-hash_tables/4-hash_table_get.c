8128a83ad075e773aa86c6b827d24feacf68c9dc
charitylefosa <charitylefosa@gmail.com>
2023-09-07 17:03:47 +0000

    attempt 1

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_get- gets a value associated with key
+*@ht: pointer to hash table
+*@key: hash key
+*
+*Return: value of element NULL if no key is found
+*
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
+}
